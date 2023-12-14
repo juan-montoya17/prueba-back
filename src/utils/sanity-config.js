d6e1c9053fccd33d21651b1f259748ea0802d293
juan montoya <78770080+juan-montoya17@users.noreply.github.com>
2023-12-13 21:52:59 -0500

    Update sanity-config.js

--- src/utils/sanity-config.js
+++ src/utils/sanity-config.js
@@ -1,5 +1,7 @@
 const {createClient} = require('@sanity/client')
+
 require('dotenv').config()
+console.log(process.env.SANITY_API_TOKEN)
  const client = createClient({
   projectId: 'zwnffgbs',
   dataset: 'production',
